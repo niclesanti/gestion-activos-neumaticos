2314a8f4d4df5c26f5db040b183e86c09ed115f1
Santiago Nicle <niclesantiago@gmail.com>
2026-09-19 11:07:34 -0300

    feat: Creación backend.

---
+++ backend/gestion-neumaticos/src/main/java/ar/edu/utn/frsf/pfc/niclemeichtry/gestion_neumaticos/GestionNeumaticosApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frsf.pfc.niclemeichtry.gestion_neumaticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNeumaticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNeumaticosApplication.class, args);
+	}
+
+}
---
+++ backend/gestion-neumaticos/src/test/java/ar/edu/utn/frsf/pfc/niclemeichtry/gestion_neumaticos/GestionNeumaticosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frsf.pfc.niclemeichtry.gestion_neumaticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNeumaticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
